64c2e5a18eab82390b0eeb830bb7f24b4be02ba7
Patrick Miller <paddymills@pm.me>
2022-04-01 16:29:37 -0400

    clean up frontend

--- svelte/src/App.svelte
+++ svelte/src/App.svelte
@@ -1,30 +1,13 @@
 <script lang="ts">
+	import { Card, Styles } from 'sveltestrap';
+
 	export let name: string;
 </script>
 
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+<main class="d-flex flex-column align-items-center p-5">
+	<Card class="p-3">
+		<p class="m-1">{name}</p>
+	</Card>
 </main>
 
-<style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
-</style>
+<Styles />
