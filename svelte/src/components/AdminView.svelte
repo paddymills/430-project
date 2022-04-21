c6a0b8a69f2fda6fdbd97de1c79f4d024515c71d
Patrick Miller <paddymills@pm.me>
2022-04-20 21:28:49 -0400

    add menu

--- svelte/src/components/AdminView.svelte
+++ svelte/src/components/AdminView.svelte
@@ -1,8 +1,60 @@
 <script lang="ts">
 	import { slide } from 'svelte/transition';
+	import { Button, Icon } from 'sveltestrap';
 	import Table from './Table.svelte';
+
+	import { invoke } from '@tauri-apps/api/tauri';
+
+	enum State {
+		Menu,
+		Customers,
+		Loans
+	};
+
+	let state = State.Menu;
+	let data;
+
+	function showMenu() {
+		state = State.Menu;
+	}
+
+	function showCustomers() {
+		state = State.Customers;
+	}
+
+	function showLoans() {
+		state = State.Loans;
+	}
+
+	async function load_loans() {
+		await invoke('get_loans', {})
+			.then((result: [any]) => {
+				// console.log(result);
+
+				data = result.map(x => {
+					x.editing = false;
+
+					return x;
+				});
+
+				console.log(data);
+			})
+			.catch((error) => console.log(error));
+	}
 </script>
 
 <div transition:slide={{delay: 250, duration: 500}} class="p-5">
-	<h1 class="text-secondary">Menu goes here</h1>
+	{#if state === State.Menu}
+		<div class="d-grid gap-2 col-6 mx-auto">
+			<Button size="lg" color="primary" on:click={showCustomers}>Customers</Button>
+			<Button size="lg" color="primary" on:click={showLoans}>Loans</Button>
+		</div>
+	{:else}
+		{#if state === State.Customers}
+			<h1>Customers</h1>
+		{:else if state === State.Loans}
+			<h1>Loans</h1>
+		{/if}
+		<Button color="primary" on:click={showMenu}><Icon name="x" /> Close</Button>
+	{/if}
 </div>
